e11dc87dca644f92e7b26641e83c6551e6309fa1
Mohd Adnan Khan <Adnankhanmak1786@gmail.com>
2024-08-13 18:47:43 +0530

    Time: 22 ms (5.71%), Space: 45.2 MB (6.61%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+        int j = 0;  
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[j]) { 
+                j++;
+                nums[j] = nums[i];  
+            }
+        }
+        for (int i = 0; i <= j; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+
+        return j + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 2, 3, 3, 3, 4, 5, 5};
+        System.out.println("Number of unique elements: " + removeDuplicates(nums));
+    }
+}
