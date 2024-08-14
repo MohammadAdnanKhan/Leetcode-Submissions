76994186fad8ca9005bf845ffb4c69815732505c
Mohd Adnan Khan <Adnankhanmak1786@gmail.com>
2024-08-14 15:36:21 +0530

    Time: 1 ms (36.56%), Space: 42 MB (75.17%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -8,19 +8,12 @@ class Solution {
                 j++;
             }
         }}
-        for (int i = 0; i <nums1.length; i++) {
-            System.out.print(nums1[i]+" ");
-        }
         Arrays.sort(nums1);
-        System.out.println();
-        for (int i = 0; i <nums1.length; i++) {
-            System.out.print(nums1[i]+" ");
-        }
     }
 
     public static void main(String[] args) {
-        int [] nums1={-1,0,0,3,3,3,0,0,0};
-        int [] nums2={1,2,2};
+        int [] nums1={1};
+        int [] nums2={};
         int m=nums1.length;
         int n=nums2.length;
         merge(nums1,m,nums2,n);
