3936135991167d824bc04d77e54ef3337289951f
Mohd Adnan Khan <Adnankhanmak1786@gmail.com>
2024-08-14 16:05:14 +0530

    Time: 7 ms (30.58%), Space: 46.7 MB (5.47%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i]==nums[i+1]){
+                i++;
+            }
+            else {
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
+
+    public static void main(String[] args) {
+        int [] nums={1};
+        System.out.println(singleNumber(nums));
+    }
+}
