a08c804d951d81454860df48294810b1eea7078e
Mohd Adnan Khan <Adnankhanmak1786@gmail.com>
2024-08-14 16:32:45 +0530

    Time: 20 ms (19.84%), Space: 55.7 MB (73.46%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,4 +1,4 @@
-class Solution {
+public class Solution {
     public static boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
         for (int i = 0; i <nums.length-1; i++) {
@@ -10,7 +10,7 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        int [] nums={1,2,3,1};
-        System.out.println(containsDuplicate(nums));
+        int [] nums={1,0,1};
+        containsDuplicate(nums);
     }
 }
