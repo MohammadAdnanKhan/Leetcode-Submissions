3cfba193346fe2e0c8359c3049cfcd4afddcf3b5
Mohd Adnan Khan <Adnankhanmak1786@gmail.com>
2024-08-15 15:27:09 +0530

    Time: 1 ms (80.20%), Space: 45.5 MB (46.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -17,11 +17,7 @@ class Solution {
                 }
             }
         }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j]+" ");
-            }
-            System.out.println();}
+        
         }
     public static void main(String[] args) {
         int[][]matrix={{1,1,1},{1,0,1},{1,1,1}};
