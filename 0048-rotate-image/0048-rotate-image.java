1d5e489d203680db4459d364ad9c2065db47d44e
Mohd Adnan Khan <Adnankhanmak1786@gmail.com>
2024-08-15 12:41:00 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (24.46%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -15,12 +15,6 @@ public class Solution {
                 matrix[i][n - j - 1] = temp;
             }
         }
-        for (int i = 0; i <matrix.length; i++) {
-            for (int j = 0; j <matrix.length; j++) {
-                System.out.print(matrix[i][j]+" ");
-            }
-            System.out.println();
-        }
     }
     public static void main(String[] args) {
         int [][] matrix={{1,2,3},{4,5,6},{7,8,9}};
