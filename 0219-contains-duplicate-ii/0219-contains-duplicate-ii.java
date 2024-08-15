bd8641c14ea87f153a1d47b708f7962dd32d7789
Mohd Adnan Khan <Adnankhanmak1786@gmail.com>
2024-08-15 18:25:55 +0530

    Time: 1546 ms (5.05%), Space: 54.7 MB (97.03%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        for (int i = 0; i <nums.length; i++) {
+            for (int j = i+1; Math.abs(i-j)<=k && j < nums.length ; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int [] nums={1,2,3,1,2,3};
+        int k=2;
+        System.out.println(containsNearbyDuplicate(nums,k));
+    }
+}
