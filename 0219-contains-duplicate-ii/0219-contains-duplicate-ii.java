60372b23ca7a40c8637b9bbb6f15cd617147a4cc
Mohd Adnan Khan <Adnankhanmak1786@gmail.com>
2024-08-15 18:26:11 +0530

    Time: 1547 ms (5.05%), Space: 58.9 MB (5.12%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -9,10 +9,4 @@ public class Solution {
         }
         return false;
     }
-
-    public static void main(String[] args) {
-        int [] nums={1,2,3,1,2,3};
-        int k=2;
-        System.out.println(containsNearbyDuplicate(nums,k));
-    }
 }
