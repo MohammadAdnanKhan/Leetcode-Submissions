65f804abad3b1a685f3f4fd8235f727f47f543a8
Mohd Adnan Khan <Adnankhanmak1786@gmail.com>
2024-08-16 11:29:23 +0530

    Time: 4 ms (52.63%), Space: 53.1 MB (37.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int majorityElement(int[] nums) {
+    public static int majorityElement(int[] nums) {
         Arrays.sort(nums);
         if (nums[0]==nums[nums.length/2]){
             return nums[0];
@@ -7,5 +7,10 @@ class Solution {
             return nums[nums.length-1];
         }
         return nums[nums.length/2];
+    }
+
+    public static void main(String[] args) {
+        int [] nums={3,2,3};
+        System.out.println(majorityElement(nums));
     }
 }
