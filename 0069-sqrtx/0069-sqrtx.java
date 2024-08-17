fbe752a46aeb88646bf56d3ee6499dc7df3f4d14
Mohd Adnan Khan <Adnankhanmak1786@gmail.com>
2024-08-17 13:06:07 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (26.81%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int mySqrt(int x) {
+        int sqrt= (int) Math.sqrt(x);
+        return sqrt;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(15));
+    }
+}
