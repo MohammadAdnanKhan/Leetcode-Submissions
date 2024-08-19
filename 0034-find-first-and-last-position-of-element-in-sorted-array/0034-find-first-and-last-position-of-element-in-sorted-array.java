8ab6f202fe82f33e7f52aa28ecbddfb79ba40286
Mohd Adnan Khan <Adnankhanmak1786@gmail.com>
2024-08-19 16:24:01 +0530

    Time: 0 ms (100.00%), Space: 46 MB (30.21%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,39 @@
 public class Solution {
+    public static int firstindex(int [] nums,int target, boolean first){
+        int start=0;
+        int ans = -1;
+        int end=nums.length-1;
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            if (target>nums[mid]){
+                start=mid+1;
+            }else if (target<nums[mid]){
+                end=mid-1;
+            }else {
+                ans=mid;
+                if (first){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
+        }
+        return ans;
+    }
     public static int[] searchRange(int[] nums, int target) {
-        int [] output=new int [2];
-        if (nums.length==0){
-            output=new int[]{-1,-1};
-        }
-        for (int i = 0; i <nums.length; i++) {
-            if (nums[i]==target){
-                output[0]=i;
-                i=nums.length;
-            }
-            else{
-                output[0]=-1;
-            }
-        }
-        for (int i = nums.length-1; i >=0 ; i--) {
-            if (nums[i]==target){
-                output[1]=i;
-                i=-1;
-            }
-            else{
-                output[1]=-1;
-            }
-        }
+        int [] output=new int[2];
+
+        output[0]=firstindex(nums,target,true);
+        output[1]=firstindex(nums,target,false);
+
         return output;
     }
 
     public static void main(String[] args) {
-        int [] nums={};
-        int target=0;
-        int [] output=searchRange(nums,target);
-        for (int i = 0; i < output.length; i++) {
+        int [] nums={5,7,7,8,8,10};
+        int [] output=searchRange(nums,6);
+        for (int i = 0; i <2; i++) {
             System.out.print(output[i]+" ");
         }
-    }
-}
+    }}
