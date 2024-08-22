174d7a6b8664989eb6727a265a82f96b34987b5b
Mohd Adnan Khan <Adnankhanmak1786@gmail.com>
2024-08-22 16:00:17 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (18.07%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,16 +1,52 @@
-public class Solution {
-    public static int search(int[] nums, int target) {
-        for (int i = 0; i <nums.length; i++) {
-            if (nums[i]==target){
-                return i;
+   public class Solution {
+       public static int pivotidx(int [] arr){
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if (mid < end && arr[mid]>arr[mid+1]){
+                return mid;
+            } else if (mid >start && arr[mid]<arr[mid-1]) {
+                return mid-1;
+            } else if (arr[mid]<=arr[start]) {
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
             }
         }
         return -1;
     }
 
+    public static int ascendingbinarysearch(int [] arr,int target,int start, int end){
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            if (arr[mid]==target){
+                return mid;
+            }
+            else if (arr[mid]>target){
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+    public static int search(int[] nums, int target) {
+        int maxidx=pivotidx(nums);
+        int leftsearch=ascendingbinarysearch(nums,target,0,maxidx);
+        if (leftsearch!=-1){
+            return leftsearch;
+        }
+        else {
+            return ascendingbinarysearch(nums,target,maxidx+1, nums.length-1);
+        }
+    }
+
     public static void main(String[] args) {
-        int [] nums={1,2};
-        int target=2;
+        int [] nums={5,1,3};
+        int target=5;
         System.out.println(search(nums,target));
     }
 }
