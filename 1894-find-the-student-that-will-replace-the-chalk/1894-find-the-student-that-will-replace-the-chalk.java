38b0fe77c86389559febca67bfef8fe588bf793d
Mohd Adnan Khan <Adnankhanmak1786@gmail.com>
2024-09-02 14:26:08 +0530

    Time: 2 ms (72.59%), Space: 60.2 MB (20.62%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -1,16 +1,12 @@
 class Solution {
     public static int chalkReplacer(int[] chalk, int k) {
         long totalChalk = 0;
-        
-        // Calculate the total amount of chalk needed for one complete round
         for (int c : chalk) {
             totalChalk += c;
         }
         
-        // Reduce k to be within one full round
-        k %= totalChalk;
+        k %= totalChalk; 
         
-        // Determine which student will replace the chalk
         for (int i = 0; i < chalk.length; i++) {
             if (k < chalk[i]) {
                 return i;
@@ -18,14 +14,13 @@ class Solution {
             k -= chalk[i];
         }
         
-        // In theory, this should never be reached
-        return -1;
+        return -1; 
     }
 
     public static void main(String[] args) {
-        // Example test case
         int chalk[] = {5, 1, 5};
         int k = 22;
-        System.out.println(chalkReplacer(chalk, k)); // Output should be 0
+        System.out.println(chalkReplacer(chalk, k)); 
+    }
 }
-}
+
